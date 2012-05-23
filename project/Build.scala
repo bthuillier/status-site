6f4c458d3896ca9571d450d20a76a7fb48fd448a
Benjamin Thuillier <benjaminthuillier@gmail.com>
2012-05-23 22:23:29 +0200

    add mongo dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,13 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  	"com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+  	"com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"    
+	)
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+   resolvers ++= Seq(
+     "repo.novus snaps" at "http://repo.novus.com/snapshots/")    
+)
 
 }
