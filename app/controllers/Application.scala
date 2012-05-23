55cf0655973671e8e9c5f0ba25be63007a748afc
Benjamin Thuillier <benjaminthuillier@gmail.com>
2012-05-23 23:48:04 +0200

    add service form and beginning the implementation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import org.bson.types.ObjectId
+import models.Service
 
 object Application extends Controller {
   
+  val serviceForm = Form(
+    mapping(
+      "id" -> ignored(new ObjectId()),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText,
+      "status" -> nonEmptyText
+    )(Service.apply)(Service.unapply)
+  )
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Hello world")
   }
+  
+  def services = TODO
+  
+  def newService = TODO
+  
+  def deleteService(name: String) = TODO
   
 }
